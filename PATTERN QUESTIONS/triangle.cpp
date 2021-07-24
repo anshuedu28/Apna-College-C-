4d1ceef7fdff755e2387f54a2c64112127db660c
anshuedu28 <58930213+anshuedu28@users.noreply.github.com>
2021-07-24 18:16:12 +0530

    Add files via upload

---
+++ PATTERN QUESTIONS/triangle.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sidea,sideb,sidec;
+    cout<<"Input three sides of triangle\n";
+    cin>>sidea>>sideb>>sidec;
+    if(sidea==sideb &&  sideb==sidec)
+    {
+        cout<<"This is an equilateral triangle\n";
+    }
+    else if (sidea==sideb || sidea==sidec || sideb|| sidec)
+    {
+        cout<<"This is an isosceles triangle\n";
+    }
+    else 
+    {
+        cout<<"This is a scalene triangle";
+    }
+    return 0;
+}
